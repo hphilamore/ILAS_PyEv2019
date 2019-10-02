85c00521e64b08d58e2babc32b5d60fef78f7e47
hphilamore <hemmaphilamore@gmail.com>
2019-10-02 11:33:57 +0900

    final tidy-up before ILAS seminars

--- Examples/functions_example/file_B.py
+++
@@ -1,7 +0,0 @@
-number_B = 2
-
-def print_a_number():
-    print(number_B)    
-    
-def type_interrogate(data):
-    print(type(data))
--- Examples/functions_example/sibling_folder/file_C.py
+++
@@ -1,6 +0,0 @@
-def subtract_and_increment(a, b):
-    """"
-    Return a minus b, plus 1
-    """
-    c = a - b + 1
-    return c
